5ece06012e1c506a5eb085aac4dbc526e4aa2089
jing_zhong <49520956+jingguotaiduo@users.noreply.github.com>
2022-09-01 02:53:50 +0800

    Create one-line-to-copy-code-CSDN.js
    copy the code on the website CSDN without logging in!

---
+++ one-line-to-copy-code-CSDN.js
@@ -0,0 +1,8 @@
+/*
+🌈🌈🌈🌈🌈🌈
+Author: jing_zhong
+Date: 2022-9-1
+Function: copy the code on CSDN website without logging in!❤️❤️❤️
+*/
+document.designMode='on';//open the mode of designing the webpage
+document.designMode='off';// close the mode of designing the webpage
